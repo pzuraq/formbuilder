8b36d0f8e4b1365c62225465629d20a70935313a
pzuraq <cmg@pzuraq.co>
2014-05-13 00:06:32 -0700

    adding readme

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513060205) do
+ActiveRecord::Schema.define(version: 20140513055009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
-
-  create_table "table_tests", force: true do |t|
-    t.string "test_column"
-  end
-
-  create_table "test_models", force: true do |t|
-    t.string   "test_column"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
