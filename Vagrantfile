cdb47b634eba6d818d9ff92cdba337835d0adb74
pzuraq <cmg@pzuraq.co>
2014-05-12 22:26:33 -0700

    fuck chef, hell yea puppet

--- Vagrantfile
+++ Vagrantfile
@@ -7,13 +7,20 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # -*- mode: ruby -*-
   # vi: set ft=ruby :
-  config.vm.box      = 'hashicorp/precise32'
+  config.vm.box = 'puppetlabs/ubuntu-12.04-32-puppet'
 
   config.vm.network :forwarded_port, guest: 3000, host: 3000
 
+  config.vm.provision :shell do |shell|
+    shell.inline = "chsh -s /bin/bash vagrant;
+                    mkdir -p /etc/puppet/modules;
+                    puppet module install puppetlabs/postgresql;
+                    puppet module install alup-rbenv;
+                    puppet module install saz-memcached"
+  end
+
   config.vm.provision :puppet do |puppet|
     puppet.manifests_path = 'puppet/manifests'
-    puppet.module_path    = 'puppet/modules'
   end
 
 end
---
+++ puppet/manifests/default.pp
@@ -0,0 +1,105 @@
+$ar_databases = ['activerecord_unittest', 'activerecord_unittest2']
+$as_vagrant   = 'sudo -u vagrant -H bash -l -c'
+$home         = '/home/vagrant'
+
+# Pick a Ruby version modern enough, that works in the currently supported Rails
+# versions, and for which RVM provides binaries.
+$ruby_version = '2.1.1'
+
+# Password
+$password = 'hDprK5pvXp6FavcAn6c4jUpGXScSK5SrfBFYxU2mrPuwZ2tjQfx9HA7pMu'
+
+Exec {
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+}
+
+# --- Preinstall Stage ---------------------------------------------------------
+
+stage { 'preinstall':
+  before => Stage['main']
+}
+
+class apt_get_update {
+  exec { 'apt-get -y update':
+    unless => "test -e ${home}/.rvm"
+  }
+}
+class { 'apt_get_update':
+  stage => preinstall
+}
+
+# --- PostgreSQL ---------------------------------------------------------------
+
+class install_postgres {
+  class { 'postgresql::globals':
+    encoding            => 'UTF8',
+    locale              => 'en_NG',
+    manage_package_repo => true,
+    version             => '9.3',
+  }->
+  class { 'postgresql::server': }
+  class { 'postgresql::lib::devel': }
+
+  postgresql::server::role { "vagrant":
+    superuser => true,
+  }
+
+  postgresql::server::db { 'formbuilder':
+    user => 'rails',
+    password => "${password}"
+  }
+
+  postgresql::server::db { 'formbuilder_test':
+    user => 'rails',
+    password => "${password}"
+  }
+}
+class { 'install_postgres': }
+
+# --- Memcached ----------------------------------------------------------------
+
+class { 'memcached': }
+
+# --- Packages -----------------------------------------------------------------
+
+# Stuff
+package { ['curl', 'build-essential', 'zlib1g-dev', 'libssl-dev', 'libreadline-dev']:
+  ensure => installed,
+}
+
+# Postgres dependencies
+package { 'postgresql-server-dev-9.1':
+  ensure => installed
+}
+
+# RMagick dependencies
+package { ['libmagickwand4', 'libmagickwand-dev']:
+  ensure => installed
+}
+
+# Nokogiri dependencies.
+package { ['libxml2', 'libxml2-dev', 'libxslt1-dev']:
+  ensure => installed
+}
+
+# ExecJS runtime.
+package { 'nodejs':
+  ensure => installed
+}
+
+# --- Ruby ---------------------------------------------------------------------
+
+class install_rbenv {
+  rbenv::install { 'vagrant': }
+  rbenv::compile { "${ruby_version}":
+    user => 'vagrant',
+    global => true
+  }
+}
+class { 'install_rbenv': }
+# --- Locale -------------------------------------------------------------------
+
+# Needed for docs generation.
+exec { 'update-locale':
+  command => 'update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8'
+}
