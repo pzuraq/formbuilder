7b63e308ce032955681dacfcedae8872296e9bab
livinginformation <douglas.roffel@gmail.com>
2014-05-20 12:56:17 -0700

    commit

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,4 +1,19 @@
 class Permission < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
+
+  after_create :bubble_down_promotion
+
+
+  def bubble_down_promotion
+    @children = Group.all.where(:parent_id => group.id)
+    unless @children.nil? 
+      @children.each do |child| 
+        unless child.permissions.where(:user_id => user.id, :group_id => child.id, :inherited => [false,nil])
+          Permission.create(:user_id => user.id, :group_id => child.id, :inherited => true, :role => @role)
+        end
+      end
+    end
+  end
+
 end
