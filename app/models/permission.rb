7381acb0ebd4e8cd333b8466169738b8bd0a4547
livinginformation <douglas.roffel@gmail.com>
2014-05-21 02:56:01 -0700

    commit

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,4 +2,42 @@ class Permission < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
 
+  after_create :bubble_down_promotion
+  before_destroy :bubble_down_demotion
+
+
+  def bubble_down_promotion
+    role_value = { :moderator => 3, :editor => 2, :viewer => 1 }
+    @parent_group_permission = group.permissions.where(:user => user).first
+    @parent_group_role = @parent_group_permission.role
+    group.subgroups.each do |subgroup|
+      @permission = subgroup.permissions.where(:user => user)
+      @current_group_permission = @permission.first
+      if @current_group_permission.nil?
+        subgroup.permissions.create(:user=> user, :role => @parent_group_role)
+      else
+        @current_group_role = @current_group_permission.role
+        unless role_value[@current_group_role.to_sym] > role_value[@parent_group_role.to_sym]
+          @permission.destroy_all
+          subgroup.permissions.create(:user=> user, :role => @parent_group_role)
+        end
+      end
+    end
+  end
+
+
+  def bubble_down_demotion
+    role_value = { :moderator => 3, :editor => 2, :viewer => 1 }
+    @parent_group_permission = group.permissions.where(:user => user).first
+    @parent_group_role = @parent_group_permission.role.to_sym
+    group.subgroups.each do |subgroup|
+      @permission = subgroup.permissions.where(:user => user)
+      @current_group_permission = @permission.first
+      next if @current_group_permission.nil?
+      @current_group_role       = @current_group_permission.role.to_sym
+      unless role_value[@current_group_role] > role_value[@parent_group_role]
+        @permission.destroy_all
+      end
+    end
+  end
 end
