7edf8bf3e15d1d881dec80e83057ab7b2cf8a1b9
livinginformation <douglas.roffel@gmail.com>
2014-05-29 16:50:10 -0700

    Fixes #23, Add cascading deletion of groups and forms. Change delete confirmation dialogue.

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,16 +2,16 @@ class Group < ActiveRecord::Base
 
 	belongs_to :owner, :class_name => 'User'
 	belongs_to :parent, :class_name => 'Group'
-	has_many :forms, -> { order name: :asc }
-	has_many :subgroups, -> { order name: :asc }, foreign_key: :parent_id, :class_name => 'Group'
+	has_many :forms, -> { order name: :asc }, :dependent => :delete_all
+	has_many :subgroups, -> { order name: :asc }, foreign_key: :parent_id, :class_name => 'Group', :dependent => :delete_all
 
-  has_many :permissions
+  has_many :permissions, :dependent => :delete_all
 	has_many :users, through: :permissions
 
-  has_many :moderator_permissions, -> { where role_rank: 0 }, :class_name => 'Permission'
+  has_many :moderator_permissions, -> { where role_rank: 0 }, :class_name => 'Permission', :dependent => :delete_all
   has_many :moderators, source: :user, through: :moderator_permissions
 
-  has_many :editor_permissions, -> { where role_rank: 1 }, :class_name => 'Permission'
+  has_many :editor_permissions, -> { where role_rank: 1 }, :class_name => 'Permission', :dependent => :delete_all
   has_many :editors, source: :user, through: :editor_permissions
 
 	before_create :set_supergroups
