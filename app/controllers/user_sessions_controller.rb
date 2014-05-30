cbb951386501c0484f5ce0f8fba20f2080703fd6
livinginformation <douglas.roffel@gmail.com>
2014-05-29 22:05:42 -0700

    Change homepage and redirect on login.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,13 +1,14 @@
 class UserSessionsController < ApplicationController
   before_filter :require_login, only: [:destroy]
-  
+
   def new
     @user = User.new
   end
 
   def create
     if @user = login(params[:username], params[:password])
-      redirect_to(:groups, notice: 'Login Successful')
+      @user_group = Group.where(:owner => @user, :parent_id => [false, nil]).first!
+      redirect_to(@user_group, notice: 'Login Successful')
     else
       flash.now[:alert] = 'Login Failed'
       render action: 'new'
